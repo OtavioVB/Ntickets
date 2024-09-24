3926d30db4f0c84e7d57ea85bb9fb0ab8d57ee1e
Otávio Carmanini <otaviovb.developer@gmail.com>
2024-09-23 23:25:14 -0300

    feature(NTICKETS-001): tenant infra data configuration

---
+++ src/Ntickets.Infrascructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ntickets.Infrascructure.EntityFrameworkCore;
+
+namespace Ntickets.Infrascructure;
+
+public static class DependencyInjection
+{
+    public static void ApplyInfrascructureDependenciesConfiguration(
+        this IServiceCollection serviceCollection,
+        string connectionString)
+    {
+        #region Entity Framework Core DbContext Configuration
+
+        const int DEFAULT_DB_CONTEXT_POOL_SIZE = 512;
+        const string DB_CONTEXT_ASSEMBLY_MIGRATIONS = "Ntickets.Infrascructure";
+
+        serviceCollection.AddDbContextPool<DataContext>(
+            optionsAction: options => options.UseNpgsql(
+                connectionString: connectionString,
+                npgsqlOptionsAction: npgsqlOptions => npgsqlOptions.MigrationsAssembly(
+                    assemblyName: DB_CONTEXT_ASSEMBLY_MIGRATIONS)),
+            poolSize: DEFAULT_DB_CONTEXT_POOL_SIZE);
+
+        #endregion
+    }
+}
---
+++ src/Ntickets.Infrascructure/EntityFrameworkCore/Mappings/TenantMapping.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ntickets.Domain.BoundedContexts.TenantContext.DataTransferObject;
+using Ntickets.Domain.BoundedContexts.TenantContext.Enumerators;
+using Ntickets.Domain.ValueObjects;
+
+namespace Ntickets.Infrascructure.EntityFrameworkCore.Mappings;
+
+public sealed class TenantMapping : IEntityTypeConfiguration<Tenant>
+{
+    public void Configure(EntityTypeBuilder<Tenant> builder)
+    {
+        #region Table Configuration
+
+        const string SCHEMA_NAME = "tenant";
+        const string TABLE_NAME = "tenants";
+
+        builder.ToTable(
+            name: TABLE_NAME,
+            schema: SCHEMA_NAME);
+
+        #endregion
+
+        #region Primary Key Configuration
+
+        const string PRIMARY_KEY_CONSTRAINT_NAME = "pk_tenant_id";
+
+        builder.HasKey(p => p.TenantId)
+            .HasName(PRIMARY_KEY_CONSTRAINT_NAME);
+
+        #endregion
+
+        #region Foreign Key Configuration
+
+        #endregion
+
+        #region Index Key Configuration
+
+        const string DOCUMENT_UNIQUE_KEY_CONSTRAINT_NAME = "uk_tenant_document_number";
+
+        builder.HasIndex(p => p.Document)
+            .HasDatabaseName(DOCUMENT_UNIQUE_KEY_CONSTRAINT_NAME)
+            .IsUnique(true);
+
+        const string TENANT_ID_ASCENDING_INDEX_KEY_CONSTRAINT_NAME = "ik_asc_tenant_id";
+
+        builder.HasIndex(p => p.TenantId)
+            .HasDatabaseName(TENANT_ID_ASCENDING_INDEX_KEY_CONSTRAINT_NAME)
+            .IsDescending(false);
+
+        #endregion
+
+        #region Properties Configuration
+
+        builder.Property(p => p.TenantId)
+            .IsRequired(true)
+            .IsFixedLength(true)
+            .HasColumnType("VARCHAR")
+            .HasColumnName("idtenant")
+            .HasMaxLength(Ulid.Empty.ToString().Length)
+            .ValueGeneratedNever();
+        builder.Property(p => p.CreatedAt)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnType("TIMESTAMPTZ")
+            .HasColumnName("created_at")
+            .ValueGeneratedNever();
+        builder.Property(p => p.FantasyName)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnName("fantasy_name")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(FantasyNameValueObject.MAX_LENGTH)
+            .ValueGeneratedNever();
+        builder.Property(p => p.LegalName)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnName("legal_name")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(LegalNameValueObject.MAX_LENGTH)
+            .ValueGeneratedNever();
+        builder.Property(p => p.Document)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnName("document")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(DocumentValueObject.CNPJ_LENGTH)
+            .ValueGeneratedNever();
+        builder.Property(p => p.Email)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnName("email")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(EmailValueObject.MAX_LENGTH)
+            .ValueGeneratedNever();
+        builder.Property(p => p.Phone)
+            .IsRequired(true)
+            .IsFixedLength(true)
+            .HasColumnName("phone")
+            .HasColumnType("CHAR")
+            .HasMaxLength(PhoneValueObject.EXPECTED_LENGTH)
+            .ValueGeneratedNever();
+        builder.Property(p => p.Status)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnName("status")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255)
+            .HasConversion(p => p.ToString(), p => Enum.Parse<EnumTenantStatus>(p))
+            .ValueGeneratedNever();
+        builder.Property(p => p.LastModifiedAt)
+            .IsRequired(true)
+            .IsFixedLength(false)
+            .HasColumnType("TIMESTAMPTZ")
+            .HasColumnName("last_modified_at")
+            .ValueGeneratedNever();
+
+        #endregion
+    }
+}
