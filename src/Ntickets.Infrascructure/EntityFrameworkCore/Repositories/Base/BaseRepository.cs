95deef0431e9a494deb6203c9d8416a721f5a1c7
Otávio Carmanini <otaviovb.developer@gmail.com>
2024-09-26 15:27:35 -0300

    refact(NTICKETS-001): add traces on repositories executions

--- src/Ntickets.Infrascructure/EntityFrameworkCore/Repositories/Base/BaseRepository.cs
+++ src/Ntickets.Infrascructure/EntityFrameworkCore/Repositories/Base/BaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Ntickets.BuildingBlocks.AuditableInfoContext;
+using Ntickets.BuildingBlocks.ObservabilityContext.Traces.Interfaces;
 using Ntickets.Infrascructure.EntityFrameworkCore.Repositories.Base.Interfaces;
-using System.Threading;
+using System.Diagnostics;
 
 namespace Ntickets.Infrascructure.EntityFrameworkCore.Repositories.Base;
 
@@ -8,27 +9,79 @@ public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity>
     where TEntity : class
 {
     protected readonly DataContext _dataContext;
+    protected readonly ITraceManager _traceManager;
 
-    protected BaseRepository(DataContext dataContext)
+    protected BaseRepository(
+        DataContext dataContext,
+        ITraceManager traceManager)
     {
         _dataContext = dataContext;
+        _traceManager = traceManager;
     }
 
-    public virtual Task AddAsync(TEntity entity, AuditableInfoValueObject auditableInfo,  CancellationToken cancellationToken)
-        => _dataContext.Set<TEntity>().AddAsync(entity, cancellationToken).AsTask();
-
+    public virtual Task AddAsync(TEntity entity, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entity,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => _dataContext.Set<TEntity>().AddAsync(input, cancellationToken).AsTask(),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
     public virtual Task AddRangeAsync(TEntity[] entities, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
-        => _dataContext.Set<TEntity>().AddRangeAsync(entities, cancellationToken);
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entities,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => _dataContext.Set<TEntity>().AddRangeAsync(input, cancellationToken),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
 
     public virtual Task DeleteAsync(TEntity entity, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
-        => Task.Run(() => _dataContext.Set<TEntity>().Remove(entity), cancellationToken);
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entity,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => Task.Run(() => _dataContext.Set<TEntity>().Remove(entity), cancellationToken),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
 
     public virtual Task DeleteRangeAsync(TEntity[] entities, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
-        => Task.Run(() => _dataContext.Set<TEntity>().RemoveRange(entities), cancellationToken);
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entities,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => Task.Run(() => _dataContext.Set<TEntity>().RemoveRange(entities), cancellationToken),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
 
     public virtual Task UpdateAsync(TEntity entity, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
-        => Task.Run(() => _dataContext.Set<TEntity>().Update(entity), cancellationToken);
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entity,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => Task.Run(() => _dataContext.Set<TEntity>().Update(entity), cancellationToken),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
 
     public virtual Task UpdateRangeAsync(TEntity[] entities, AuditableInfoValueObject auditableInfo, CancellationToken cancellationToken)
-        => Task.Run(() => _dataContext.Set<TEntity>().UpdateRange(entities), cancellationToken);
+        => _traceManager.ExecuteTraceAsync(
+            traceName: $"{nameof(BaseRepository<TEntity>)}.{nameof(AddAsync)}",
+            activityKind: ActivityKind.Internal,
+            input: entities,
+            handler: (input, auditableInfo, activity, cancellationToken)
+                => Task.Run(() => _dataContext.Set<TEntity>().UpdateRange(entities), cancellationToken),
+            auditableInfo: auditableInfo,
+            cancellationToken: cancellationToken,
+            keyValuePairs: []);
+        
 }
