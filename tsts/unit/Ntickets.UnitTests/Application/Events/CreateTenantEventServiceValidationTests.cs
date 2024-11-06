bdb3e45c3fb455d17ca6316af93704852be353ff
Otávio Carmanini <otaviovb.developer@gmail.com>
2024-11-06 09:01:52 -0300

    feature(NTICKETS-001): unit tests on logging information about crete tenant event service

--- tsts/unit/Ntickets.UnitTests/Application/Events/CreateTenantEventServiceValidationTests.cs
+++ tsts/unit/Ntickets.UnitTests/Application/Events/CreateTenantEventServiceValidationTests.cs
@@ -51,4 +51,49 @@ public sealed class CreateTenantEventServiceValidationTests
                 It.IsAny<CancellationToken>()),
             Times.Once);
     }
+
+    [Fact]
+    public async Task Given_Throws_Exception_On_Request_To_Create_Tenant_Event_Should_Not_Publish_Event_Message_On_ApacheKafka_And_Register_Log()
+    {
+        // Arrange
+        const string EXPECTED_TOPIC_NAME = "CREATE_TENANT_EVENT";
+        const string EXPECTED_LOGGING_MESSAGE = "Is not possible to publish event on apache kafka topic.";
+
+        var producerMock = new Mock<IApacheKafkaProducer>();
+        producerMock.Setup(p => p.PublishAsync(
+            It.IsAny<string>(),
+            It.IsAny<string>(),
+            It.IsAny<CreateTenantEvent>(),
+            It.IsAny<CancellationToken>())).ThrowsAsync(new Exception());
+
+        var loggerMock = new Mock<ILogger<CreateTenantEventService>>();
+
+        var resiliencePipelineWrapper = ResiliencePipelineWrapper.Build(
+            resiliencePipelineDefinitionName: string.Empty,
+            options: FakerResiliencePipelineWrapperOptions.CreateInstance());
+
+        var eventService = new CreateTenantEventService(
+            producer: producerMock.Object,
+            logger: loggerMock.Object,
+            resiliencePipeline: resiliencePipelineWrapper,
+            traceManager: FakerTraceManager.CreateInstance());
+
+        var @event = FakerCreateTenantEvent.CreateInstance();
+
+        // Act
+        await eventService.PublishEventAsync(
+            @event: @event,
+            auditableInfo: AuditableInfoValueObject.Factory(Guid.NewGuid().ToString()),
+            cancellationToken: CancellationToken.None);
+
+        // Assert
+        loggerMock.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(level => level == LogLevel.Error),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains(EXPECTED_TOPIC_NAME) && v.ToString()!.Contains(EXPECTED_LOGGING_MESSAGE)),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()!),
+            Times.Once);
+    }
 }
